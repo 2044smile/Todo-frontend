65e3186b7b210bdc89bc50a9b4f62198a64b2a53
2044smile <2044smile@naver.com>
2020-08-10 23:09:13 +0900

    feat: vuetify dialog working

--- todo/src/components/TodoContent.vue
+++ todo/src/components/TodoContent.vue
@@ -17,27 +17,31 @@
               <v-text-field v-model="data.description" :counter="255" label="Content" required></v-text-field>
             </v-col>
           </v-row>
-            <v-menu
-              v-model="showPicker"
-              :close-on-content-click="false" 
-              transition="scale-transition"
-              offset-y
-              max-width="500px"
-              min-width="500px"
-            >
-              <template v-slot:activator="{ on }">
-                <v-text-field
-                  v-model="data.due_date"
-                  label="Due Date"
-                  hint="YYYY-MM-DD"
-                  persistent-hint
-                  readonly
-                  v-on="on"
-                ></v-text-field>
-                <v-date-picker v-model="data.due_date" no-title @input="showPicker = false"></v-date-picker>
-              </template>
-            </v-menu>
-            <!-- <v-date-picker v-model="data.due_date" locale="ko" no-title></v-date-picker> -->
+          <v-menu
+            v-model="showPicker"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            max-width="500px"
+            min-width="500px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="data.due_date"
+                label="Due Date"
+                hint="YYYY-MM-DD"
+                persistent-hint
+                readonly
+                v-on="on"
+              ></v-text-field>
+              <v-date-picker
+                style="background-color: gray"
+                v-model="data.due_date"
+                @input="showPicker = false"
+              ></v-date-picker>
+            </template>
+          </v-menu>
+          <!-- <v-date-picker v-model="data.due_date" locale="ko" no-title></v-date-picker> -->
           <v-btn @click="sendForm" color="#4CAF50">create</v-btn>
           <v-btn @click="clearForm" color="#F44336">clear</v-btn>
         </v-flex>
@@ -48,6 +52,66 @@
                 <v-list-item-title>{{ data.title }}</v-list-item-title>
                 <v-list-item-subtitle>{{ data.description }}</v-list-item-subtitle>
               </v-list-item-content>
+              <div data-app>
+                <v-dialog v-model="dialog" persistent max-width="600px">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn color="#FFC107" v-bind="attrs" v-on="on">Update</v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title>
+                      <span class="headline">Modifying TodoList</span>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="propsdata[index].id" label="Title*"></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="data.author" label="Author*" required></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field
+                              v-model="data.description"
+                              label="Content*"
+                              hint="write down the deatils."
+                              persistent-hint
+                              required
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12">
+                            <v-text-field
+                              v-model="data.due_date"
+                              label="Due Date*"
+                              hint="YYYY-MM-DD"
+                              persistent-hint
+                              required
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6">
+                            <v-autocomplete :items="['Done', 'Not Yet']" label="Completed"></v-autocomplete>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="dialog = false"
+                        style="color:#F44336"
+                      >Close</v-btn>
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="dialog = false"
+                        style="color:#4CAF50"
+                      >Save</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </div>
               <v-btn class="ma-2" @click="deleteTodo(data.id)" color="#F44336">Delete</v-btn>
             </v-list-item>
           </v-card>
@@ -64,14 +128,15 @@ let url = "http://localhost:8000/api/todo/";
 export default {
   data: () => {
     return {
+      dialog: false,
       showPicker: false,
       data: {
         id: "",
         title: "",
         author: "",
         description: "",
-        due_date: new Date().toISOString().substr(0, 10),
-      },
+        due_date: new Date().toISOString().substr(0, 10)
+      }
     };
   },
   props: ["propsdata"],
@@ -99,7 +164,7 @@ export default {
     deleteTodo: function(id) {
       axios({
         method: "DELETE",
-        url: url + id,
+        url: url + id
       })
         .then(response => {
           this.$emit("deleted");
