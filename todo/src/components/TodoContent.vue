ead0e50fc58d1cc95802a1188c4a58c31cda21e2
2044smile <2044smile@naver.com>
2020-08-26 13:28:36 +0900

    feat: comment

--- todo/src/components/TodoContent.vue
+++ todo/src/components/TodoContent.vue
@@ -50,13 +50,13 @@
         <v-flex class="todoList" column>
           <v-card fluid>
             <v-list-item v-for="(data, index) in propsdata" v-bind:key="index">
-              <v-list-item-content v-show="!data.is_hidden">
+              <v-list-item-content v-show="!data.is_hidden">  <!-- Not 연산을 수행하기 때문에 !data.is_hidden = true 로 화면에 보이게 됩니다 -->
                 <v-list-item-title>{{ data.title }}</v-list-item-title>
                 <v-list-item-title>{{ data.author }}</v-list-item-title>
                 <v-list-item-title>{{ data.due_date }}</v-list-item-title>
                 <v-list-item-subtitle>{{ data.description }}</v-list-item-subtitle>
               </v-list-item-content>
-              <v-form v-show="data.is_hidden">
+              <v-form v-show="data.is_hidden"> <!-- is_hidden=true 가 되면 입력할 수 있는 form 이 보여집니다. -->
                 <v-container>
                   <v-row>
                     <v-col cols="12" md="6">
@@ -89,13 +89,17 @@
                     ></v-checkbox>
                     </v-col>
                     <v-col cols="12" md="4">
+                      <!-- Save 버튼은 Update 버튼을 클릭하면 볼 수 있고, Save 버튼 클릭 시 is_hidden=false 를 전달합니다. -->
                       <v-btn class="ma-2" @click="data.is_hidden = !data.is_hidden ; updateTodo(data)" v-show="data.is_hidden" color="#4CAF50">Save</v-btn>
+                      <!-- Delete 버튼은 Update 버튼을 클릭하면 볼 수 있고, Delete 버튼 클릭 시 선택 된 객체를 삭제합니다. -->
                       <v-btn class="ma-2" @click="deleteTodo(data.id)" color="#F44336">Delete</v-btn>
                     </v-col>
                   </v-row>
                 </v-container>
               </v-form>
+              <!-- Update 버튼 클릭 시 onlyTodoListCard 예제에 보이는 하나의 객체만 띄울 수 있는 메소드에 데이터를 전달합니다. -->
               <v-btn class="ma-2" @click="data.is_hidden = !data.is_hidden ; onlyTodoListCard(data, propsdata)" v-show="!data.is_hidden" color="#F9A825">Update</v-btn>
+              <!-- Delete 버튼 클릭 시 선택한 객체가 삭제 됩니다. -->
               <v-btn class="ma-2" @click="deleteTodo(data.id)" v-show="!data.is_hidden" color="#F44336">Delete</v-btn>
             </v-list-item>
           </v-card>
@@ -170,14 +174,9 @@ export default {
           console.log("Failed to patched todoList", error.response);
         });
     },
-    onlyTodoListCard: function(data, propsdata) {
-      console.log(`This is data: ${data}`);
-      console.log(`This is propsdata: ${propsdata}`);
+    onlyTodoListCard: function(data, propsdata) { // 한 개의 투두리스트만 입력 form이 보이게끔
       for (var index in propsdata) {
         (data.id != propsdata[index].id) ? propsdata[index].is_hidden = false : ''
-        // if (data.id != propsdata[index].id) {
-          // propsdata[index].is_hidden = false
-        // }
       }
     }
   }
