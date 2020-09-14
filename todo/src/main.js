70c22cd69fbc1125a90239a95489a2cad430b51c
2044smile <2044smile@naver.com>
2020-09-14 23:12:53 +0900

    feat: install vue js modal

--- todo/src/main.js
+++ todo/src/main.js
@@ -7,11 +7,13 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VModal from "vue-js-modal";
 
 library.add(fas)
 library.add(far)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(VModal, { dynamic: true})
 
 Vue.config.productionTip = false
 
